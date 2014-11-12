47b832772a548a7b4ca374f0c7555191dd460f40
Jay Wengrow <jaywngrw@gmail.com>
2014-11-12 17:45:40 -0600

    display user friendly show page

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,7 @@
 class EmployeesController < ApplicationController
   def show
-    @employee = Unirest.get("http://localhost:3000/employees/#{params[:id]}.json").raw_body
+    @employee = Unirest.get("http://localhost:3000/employees/#{params[:id]}.json").body
+    puts "EMPloueee"
+    puts @employee
   end
 end
