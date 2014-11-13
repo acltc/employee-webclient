e62b6c835f7a5f46bb81fbe672efa163b88cdbd5
Jay Wengrow <jaywngrw@gmail.com>
2014-11-13 17:04:39 -0600

    implemented index action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,11 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Unirest.get("http://localhost:3000/employees.json").body
+  end
+
   def show
     @employee = Unirest.get("http://localhost:3000/employees/#{params[:id]}.json").body
-    puts "EMPloueee"
-    puts @employee
   end
+
+
 end
